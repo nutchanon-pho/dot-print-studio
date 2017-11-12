9bedbd5f9937aac35e5ec4fe05704a58837e3eac
Nutchanon Phongoen <nutchanon.pho@gmail.com>
2017-11-12 13:13:43 +0700

    Change how Menu is displayed in responsive design

--- app/containers/Menu/index.js
+++ app/containers/Menu/index.js
@@ -6,7 +6,7 @@
 
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
-import { Menu, Image } from 'semantic-ui-react';
+import { Menu, Image, Responsive } from 'semantic-ui-react';
 import DotPrintLogo from 'images/dotprint-logo.png';
 
 const paddingForItems = { paddingRight: '75px' };
@@ -20,8 +20,13 @@ export default class DotPrintMenu extends Component {
     const { activeItem } = this.state;
     return (
       <div>
-        <Image src={DotPrintLogo} style={{ width: '150px', position: 'absolute', marginTop: '35px', marginLeft: '55px' }} />
-        <Menu style={{ paddingTop: '55px', paddingRight: '100px' }} text size="large" floated="right">
+        <Responsive {...Responsive.onlyMobile}>
+          <Image src={DotPrintLogo} style={{ width: '150px' }} centered />
+        </Responsive>
+        <Responsive {...Responsive.onlyComputer}>
+          <Image src={DotPrintLogo} style={{ width: '150px', position: 'absolute', marginTop: '35px', marginLeft: '55px' }} />
+        </Responsive>
+        <Menu stackable style={{ paddingTop: '55px', paddingRight: '100px' }} text size="large" floated="right">
           <Menu.Item style={paddingForItems} name="home" active={activeItem === 'home'} onClick={this.handleItemClick}>
             <b>HOME</b>
           </Menu.Item>
@@ -30,7 +35,7 @@ export default class DotPrintMenu extends Component {
           <Menu.Item style={paddingForItems} content="INSPIRATION" name="inspiration" active={activeItem === 'inspiration'} onClick={this.handleItemClick} />
           <Menu.Item style={paddingForItems} content="OUR STORY" name="ourStory" active={activeItem === 'ourStory'} onClick={this.handleItemClick} />
           <Menu.Item content="LOGIN" name="login" active={activeItem === 'login'} onClick={this.handleItemClick} />
-          <Menu.Item content="|" />
+          <Responsive {...Responsive.onlyComputer}><Menu.Item content="|" /></Responsive>
           <Menu.Item content="REGISTER" name="register" active={activeItem === 'register'} onClick={this.handleItemClick} />
         </Menu>
         <div style={{ clear: 'both' }} />
