cd6436cd24b26e5801eaaf7d2b63705001cb533a
Nutchanon Phongoen <nutchanon.pho@gmail.com>
2017-11-13 21:31:20 +0700

    Add thai language i18n

--- app/i18n.js
+++ app/i18n.js
@@ -7,18 +7,22 @@
 import { addLocaleData } from 'react-intl';
 import enLocaleData from 'react-intl/locale-data/en';
 import deLocaleData from 'react-intl/locale-data/de';
+import thLocaleData from 'react-intl/locale-data/th';
 
 import { DEFAULT_LOCALE } from '../app/containers/App/constants';
 
 import enTranslationMessages from './translations/en.json';
 import deTranslationMessages from './translations/de.json';
+import thTranslationMessages from './translations/th.json';
 
 addLocaleData(enLocaleData);
 addLocaleData(deLocaleData);
+addLocaleData(thLocaleData);
 
 export const appLocales = [
   'en',
   'de',
+  'th',
 ];
 
 export const formatTranslationMessages = (locale, messages) => {
@@ -36,4 +40,5 @@ export const formatTranslationMessages = (locale, messages) => {
 export const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
   de: formatTranslationMessages('de', deTranslationMessages),
+  th: formatTranslationMessages('th', thTranslationMessages),
 };
