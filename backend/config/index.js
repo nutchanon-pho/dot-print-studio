53f00cadb82efec4363eddee5eba6672073297ce
Nutchanon Pho-ngoen <>
2018-01-06 15:26:44 +0700

    Docker fix (not complete)

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,7 +1,9 @@
 import nconf from 'nconf';
-
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
     nconf.file({ file: './config/config.json' });
+} else if (process.env.NODE_ENV === 'docker') {
+    nconf.file({ file: './config/docker-config.json' });
 }
 
 module.exports = nconf;
